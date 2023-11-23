74290a1c6e89de218a519593efc51f774a86b888
Cecilia Dominguez <ceciidom98@gmail.com>
2023-11-22 20:27:44 -0600

    Styles included. One button sends random books from all lists.

--- index.js
+++ index.js
@@ -9,26 +9,13 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let listNamesEncoded = [];
-let currentLists = [];
+const API_KEYS = "YrMRJHA6VyOI21DPcRUlkW0wPeOD5V3C";
+
 
 app.get ("/", async (req, res) => {
      
     try {
-        const result = await axios.get(
-          "https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=YrMRJHA6VyOI21DPcRUlkW0wPeOD5V3C"
-        );
-        const response = result.data;
-        const lists = response.results.lists
-
-        //Push each list_name_encoded from the list array to the current Lists
-        lists.forEach(element => {
-            currentLists.push(element.display_name);
-            listNamesEncoded.push(element.list_name_encoded);
-        });
-       // console.log(listNamesEncoded);
-        res.render ("index.ejs" , {currentLists : currentLists});
-
+        res.render ("index.ejs");
     } catch (error) {
         console.log(error)
         res.status(500);
@@ -37,22 +24,29 @@ app.get ("/", async (req, res) => {
 
 //get method for random book title generator
 app.get ("/randomBookTitle", async (req, res) => {
-   
-   
-   // const clickedButton = req.id
     try {
         const result = await axios.get(
-          `https://api.nytimes.com/svc/books/v3/lists/combined-print-and-e-book-fiction.json?api-key=YrMRJHA6VyOI21DPcRUlkW0wPeOD5V3C`
+          `https://api.nytimes.com/svc/books/v3/lists/full-overview.json?api-key=${API_KEYS}`
         );
-        const bookList = result.data.results.books;
-        const randomNumber = Math.floor(Math.random() * bookList.length);
-        const randomTitle = bookList[randomNumber].title;
-        const randomAuthor = bookList[randomNumber].author;
-        const randomDescription = bookList[randomNumber].description;
-        const randomPicture = bookList[randomNumber].book_image;
+        
+        const data = result.data;
+
+        const allLists = data.results.lists
+        const randomListNumber = Math.floor(Math.random() * allLists.length);
+        const selectedList = allLists[randomListNumber];
 
 
-        res.render("index.ejs", {title: randomTitle, author: randomAuthor, description: randomDescription, image: randomPicture, currentLists: currentLists})
+        const allBooks = selectedList.books;
+        const randomBookNumber = Math.floor(Math.random() * allBooks.length);
+        const selectedBook = allBooks[randomBookNumber];
+        
+        let { display_name } = selectedList;
+        let {title , author , description , book_image, buy_links} = selectedBook;
+        let buyLink = buy_links[0].url;
+           
+
+
+        res.render("index.ejs", {listName: display_name , title: title, author: author, description: description, image: book_image, buy : buyLink})
     } catch (error) {
       console.log(error);
       res.status(500);
